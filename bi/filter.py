a78f9bb26d1b80e453902d164488b6a4f44d3994
yasarc4 <yasar.c4@gmail.com>
2017-05-03 17:56:34 +0530

    Filter using config

--- bi/filter.py
+++ bi/filter.py
@@ -10,6 +10,10 @@ from bi.common import BIException
 from parser import configparser
 from bi.common import MetaDataHelper
 import commands
+import pandas as pd
+import random
+from pyspark.sql.functions import col,udf
+from pyspark.sql.types import FloatType
 
 
 '''
@@ -34,31 +38,31 @@ if __name__ == '__main__':
 
     dff_context = FilterContextSetter(config_obj)
     dff_context.set_params()
-
-    df = DataLoader.load_csv_file(spark, dff_context.get_input_file())
-    print "FILE LOADED: ", dff_context.get_input_file()
+    ip = dff_context.get_input_file()
+    dummy_ip = ip+str(random.random())
+    df = DataLoader.load_csv_file(spark, ip)
+    print "FILE LOADED: ", ip
     dff_helper = DataFilterHelper(df, dff_context)
     dff_helper.set_params()
     df = dff_helper.get_data_frame()
 
     meta_helper = MetaDataHelper(df)
-    #meta_helper.get_datetime_suggestions()
-    if meta_helper.has_date_suggestions():
-        meta_helper.get_formats()
     CSV_FILE = dff_context.get_input_file()
     print "File loaded: ", CSV_FILE
     meta_data = utils.as_dict(meta_helper)
     print "Metadata: ", meta_data
     print '-'*20
     RESULT_FILE = dff_context.get_result_file()
-    #DataWriter.write_dict_as_json(spark, meta_data, RESULT_FILE)
+    DataWriter.write_dict_as_json(spark, meta_data, RESULT_FILE)
     #df.write.csv('hdfs://localhost:9000/input/created_by_spark.csv')
-    df.coalesce(1).write.format('csv').save('hdfs://localhost:9000/input/created_by_spark1')
-    files = commands.getoutput('hadoop fs -ls hdfs://localhost:9000/input/created_by_spark1').splitlines()[-1]
+    df.coalesce(1).write.format('csv').save(dummy_ip, header = True)
+    files = commands.getoutput('hadoop fs -ls '+dummy_ip).splitlines()[-1]
     files = files.split()[-1]
     #commands.getoutput('hadoop fs -rm '+CSV_FILE)
-    import random
     print '*'*20 + 'hadoop fs -distcp '+files+' /input/spark_created_' + '*'*20
-    commands.getoutput('hadoop distcp '+files+' /input/spark_created_'+str(random.random()))
-    commands.getoutput('hadoop fs -rm -r hdfs://localhost:9000/input/created_by_spark1')
+    #commands.getoutput('hadoop distcp '+files+' /input/spark_created_'+str(random.random()))
+    commands.getoutput('hadoop fs -rm '+CSV_FILE)
+    commands.getoutput('hadoop fs -cp '+files+' '+CSV_FILE)
+    commands.getoutput('hadoop fs -rm -r '+dummy_ip)
+
     spark.stop()
