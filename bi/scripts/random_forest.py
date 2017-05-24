aeb6a8c59996bf84a0ac5a047b5f60fde7f34f77
gulshan-gaurav <gulshan.gaurav@marlabs.com>
2017-05-24 19:47:14 +0530

    reverted

--- bi/scripts/random_forest.py
+++ bi/scripts/random_forest.py
@@ -58,7 +58,7 @@ class RandomForestScript:
         # confusion matrix keys are the predicted class
         self._model_summary["confusion_matrix"] = MLUtils.calculate_confusion_matrix(objs["actual"],objs["predicted"])
         self._model_summary["feature_importance"] = objs["feature_importance"]
-        self._model_summary["model_accuracy"] = round(metrics.accuracy_score(objs["actual"], objs["predicted"])*100,2)
+        self._model_summary["model_accuracy"] = round(metrics.accuracy_score(objs["actual"], objs["predicted"]),2)
         self._model_summary["runtime_in_seconds"] = round((time.time() - st),2)
 
         overall_precision_recall = MLUtils.calculate_overall_precision_recall(objs["actual"],objs["predicted"])
