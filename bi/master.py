9e5dbf14f6ed926770cd55e391050ef89681cb9c
yasarc4 <yasar.c4@gmail.com>
2017-07-20 15:28:21 +0530

    passing empty cards when analysis not run

--- bi/master.py
+++ bi/master.py
@@ -115,6 +115,7 @@ def main(confFilePath):
                 send_message_API(monitor_api, "ChiSquare", "ChiSquare Done", True, 100)
             except Exception as e:
                 print "ChiSquare Analysis Failed "
+                DataWriter.write_dict_as_json(spark, {'narratives':{'main_card':{},'card':[]}}, dataframe_context.get_narratives_file()+'ChiSquare/')
                 send_message_API(monitor_api, "ChiSquare", "ChiSquare Failed", False, 0)
                 print "ERROR"*5
                 print e
