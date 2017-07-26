7407a014156d1d76e2b0cdb8aa8ccfe1bb7f07dd
yasarc4 <yasar.c4@gmail.com>
2017-07-26 17:01:26 +0530

    chisquare

--- bi/master.py
+++ bi/master.py
@@ -109,19 +109,19 @@ def main(confFilePath):
             print "Descriptive analysis Not in Scripts to run "
 
         if ('Dimension vs. Dimension' in scripts_to_run):
-            try:
-                fs = time.time()
-                chisquare_obj = ChiSquareScript(df, df_helper, dataframe_context, spark)
-                chisquare_obj.Run()
-                print "ChiSquare Analysis Done in ", time.time() - fs, " seconds."
-                send_message_API(monitor_api, "ChiSquare", "ChiSquare Done", True, 100)
-            except Exception as e:
-                print "ChiSquare Analysis Failed "
-                DataWriter.write_dict_as_json(spark, {'narratives':{'main_card':{},'cards':[]}}, dataframe_context.get_narratives_file()+'ChiSquare/')
-                send_message_API(monitor_api, "ChiSquare", "ChiSquare Failed", False, 0)
-                print "#####ERROR#####"*5
-                print e
-                print "#####ERROR#####"*5
+            # try:
+            fs = time.time()
+            chisquare_obj = ChiSquareScript(df, df_helper, dataframe_context, spark)
+            chisquare_obj.Run()
+            print "ChiSquare Analysis Done in ", time.time() - fs, " seconds."
+            send_message_API(monitor_api, "ChiSquare", "ChiSquare Done", True, 100)
+            # except Exception as e:
+            #     print "ChiSquare Analysis Failed "
+            #     DataWriter.write_dict_as_json(spark, {'narratives':{'main_card':{},'cards':[]}}, dataframe_context.get_narratives_file()+'ChiSquare/')
+            #     send_message_API(monitor_api, "ChiSquare", "ChiSquare Failed", False, 0)
+            #     print "#####ERROR#####"*5
+            #     print e
+            #     print "#####ERROR#####"*5
         else:
             DataWriter.write_dict_as_json(spark, {'narratives':{'main_card':{},'cards':[]}}, dataframe_context.get_narratives_file()+'ChiSquare/')
             print "Dimension vs. Dimension Not in Scripts to run "
