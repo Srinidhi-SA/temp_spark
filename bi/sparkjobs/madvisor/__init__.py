f8f20ed8b52bbf3d748abbb0e12c80f823d93dbb
gulshan-gaurav <gulshan.gaurav@marlabs.com>
2017-08-09 17:31:42 +0530

    minor changes

--- bi/sparkjobs/madvisor/__init__.py
+++ bi/sparkjobs/madvisor/__init__.py
@@ -9,12 +9,11 @@ class JobScript(SparkJob):
         if config.get('cfgpath', None):
             job_data = config.get('cfgpath')
         else:
-        problems.append('Missing cfgpath data')
-        
-	if len(problems) == 0:
+            problems.append('Missing cfgpath data')
+	    if len(problems) == 0:
             return job_data
         else:
-	    return build_problems(problems)
+            return build_problems(problems)
 
     def run_job(self, context, runtime, data):
         confPath = data[0]
