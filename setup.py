982f074f34db6eeab60f5898e88c874813f2472c
gulshan-gaurav <gulshan.gaurav@marlabs.com>
2017-12-14 13:28:15 +0530

    added manifest.in and updated setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,10 @@ setup(
         name='marlabs-bi-jobs',
         version=os.getenv('SJS_VERSION', '0.0.0'),
         description='Marlabs BI jobs for Spark Job Server',
+        include_package_data=True,
         #url='https://github.com/spark-jobserver/spark-jobserver',
         #license='Apache License 2.0',
         packages=find_packages(exclude=['test*','example*','config*','doc*','spark-ware*']),
-	#install_requires=['numpy','scipy','pyhocon', 'py4j','pandas','statsmodels','jinja2','pattern']
-	install_requires=['numpy','scipy','pyhocon','pandas','statsmodels','jinja2','pattern']
+        #install_requires=['numpy','scipy','pyhocon', 'py4j','pandas','statsmodels','jinja2','pattern']
+        install_requires=['numpy','scipy','pyhocon','pandas','statsmodels','jinja2','pattern']
 )
