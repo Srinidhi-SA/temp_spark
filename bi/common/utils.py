dce93435726d4cf5929a9d0ee666686e7203392a
gulshan-gaurav <gulshan.gaurav@marlabs.com>
2017-12-27 16:26:44 +0530

    matplotlib issue

--- bi/common/utils.py
+++ bi/common/utils.py
@@ -11,6 +11,8 @@ import md5
 
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from numpy import size, zeros, where, transpose
 from numpy.random import normal
