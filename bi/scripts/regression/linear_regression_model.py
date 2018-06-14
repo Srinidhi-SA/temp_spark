37a493719a86be8e2de50044d1abddb58000a9e3
gulshan-gaurav <gulshan.gaurav@marlabs.com>
2018-06-14 18:26:39 +0530

    dsa

--- bi/scripts/regression/linear_regression_model.py
+++ bi/scripts/regression/linear_regression_model.py
@@ -576,7 +576,6 @@ class LinearRegressionModelScript:
         df_helper = DataFrameHelper(spark_scored_df, self._dataframe_context,self._metaParser)
         df_helper.set_params()
         df = df_helper.get_data_frame()
-        # self._dataframe_context.set_dont_send_message(True)
         try:
             fs = time.time()
             descr_stats_obj = DescriptiveStatsScript(df, df_helper, self._dataframe_context, self._result_setter, self._spark,self._prediction_narrative,scriptWeight=self._scriptWeightDict,analysisName="Descriptive analysis")
