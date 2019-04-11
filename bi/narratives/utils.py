8337ec0842486cbdf52762fa02f59438ad08c215
sumeetshah <sumeet.shah@marlabs.com>
2019-04-11 14:35:09 +0530

    Fix #1096 - Donut chart info and Prediction rules frequency mismatch.

--- bi/narratives/utils.py
+++ bi/narratives/utils.py
@@ -1063,8 +1063,6 @@ def generate_rules(colname,target,rules, total, success, success_percent,analysi
         print "<>"*76
         print "NARRATIVE - ", narrative
         print "?"*76
-        print "CRUDE NARRATIVE - ", crude_narrative
-        print "<>"*76
         return narrative,crude_narrative
 
 def statistical_info_array_formatter(st_array):
