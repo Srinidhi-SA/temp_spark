537c464a0da8fe64e2f915fc0b1215d61ff3c2df
Keshav <keshav.shrikant@marlabs.com>
2019-04-15 17:40:43 +0530

    Fixes #1072

--- bi/common/dataloader.py
+++ bi/common/dataloader.py
@@ -13,7 +13,8 @@ class DataLoader:
     @staticmethod
     @accepts(SparkSession, basestring, has_header=bool, interpret_data_schema=bool)
     def load_csv_file(spark_session, csv_file_path, has_header=True, interpret_data_schema=True):
-        return spark_session.read.csv(csv_file_path, header=has_header, inferSchema=interpret_data_schema)
+        return spark_session.read.csv(csv_file_path, header=has_header, inferSchema=interpret_data_schema, escape='"',
+                                      multiLine=True, ignoreTrailingWhiteSpace=True, nullValue='na')
 
     @staticmethod
     @accepts(SparkSession, basestring, dict)
