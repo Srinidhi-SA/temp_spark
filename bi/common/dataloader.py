d17c065989d3631eed8c6a9905ad0bea05131cb8
makashthakur <akash.thakur@marlabs.com>
2019-05-24 13:53:10 +0530

    removed file:// from data loader

--- bi/common/dataloader.py
+++ bi/common/dataloader.py
@@ -14,7 +14,7 @@ class DataLoader:
     @staticmethod
     @accepts(SparkSession, basestring, has_header=bool, interpret_data_schema=bool)
     def load_csv_file(spark_session, csv_file_path, has_header=True, interpret_data_schema=True):
-        return spark_session.read.csv("file://"+csv_file_path, header=has_header, inferSchema=interpret_data_schema, escape='"',
+        return spark_session.read.csv(csv_file_path, header=has_header, inferSchema=interpret_data_schema, escape='"',
                                       multiLine=True, ignoreTrailingWhiteSpace=True, nullValue=None and 'na' and 'nan')
 
     @staticmethod
