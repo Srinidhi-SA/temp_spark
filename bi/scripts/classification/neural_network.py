d1ca7226082c5401638513ecfbd29b0d0d6f6654
sivavamsi <sivavamsi.poka@marlabs.com>
2019-11-29 12:43:32 +0530

    #1396 fixed

--- bi/scripts/classification/neural_network.py
+++ bi/scripts/classification/neural_network.py
@@ -224,7 +224,8 @@ class NeuralNetworkScript:
                     modelmanagement_=clfRand.get_params()
                     bestEstimator = None
             else:
-                evaluationMetricDict = {"name":GLOBALSETTINGS.CLASSIFICATION_MODEL_EVALUATION_METRIC}
+                #evaluationMetricDict = {"name":GLOBALSETTINGS.CLASSIFICATION_MODEL_EVALUATION_METRIC}
+                evaluationMetricDict =algoSetting.get_evaluvation_metric(Type="CLASSIFICATION")
                 evaluationMetricDict["displayName"] = GLOBALSETTINGS.SKLEARN_EVAL_METRIC_NAME_DISPLAY_MAP[evaluationMetricDict["name"]]
                 self._result_setter.set_hyper_parameter_results(self._slug,None)
                 algoParams = algoSetting.get_params_dict()
