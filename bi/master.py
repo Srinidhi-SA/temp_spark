61cd59361f90b60d33fcc40fc69162f85f844f1e
makashthakur <akash.thakur@marlabs.com>
2020-02-27 15:34:44 +0530

    fixed the stock sense issue

--- bi/master.py
+++ bi/master.py
@@ -409,14 +409,14 @@ def submit_job_through_yarn():
         print('Main Method Did Not End ....., ', str(e))
 if __name__ == '__main__':
     jobURL, killURL = killer_setting(sys.argv[1])
-    # try:
-    main(sys.argv[1])
-    print('Main Method End .....')
-    # except Exception as e:
-    #     # print jobURL, killURL
-    #     data = {"status": "killed", "jobURL": jobURL}
-    #     resp = send_kill_command(killURL, data)
-    #     while str(resp.text) != '{"result": "success"}':
-    #         data = {"status": "killed", "jobURL": jobURL}
-    #         resp = send_kill_command(killURL, data)
-    #     print('Main Method Did Not End ....., ', str(e))
+    try:
+        main(sys.argv[1])
+        print('Main Method End .....')
+    except Exception as e:
+        # print jobURL, killURL
+        data = {"status": "killed", "jobURL": jobURL}
+        resp = send_kill_command(killURL, data)
+        while str(resp.text) != '{"result": "success"}':
+            data = {"status": "killed", "jobURL": jobURL}
+            resp = send_kill_command(killURL, data)
+        print('Main Method Did Not End ....., ', str(e))
