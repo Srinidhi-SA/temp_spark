6f60b309e1f88cc6c65b3e2204c1eb1c9e2ab04e
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-03-02 14:32:19 +0530

    Build file removed

--- build/lib/bi/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from bi.validator import Validator
-
-__all__ = [
-        'Validator',
-]
--- build/lib/bi/tests/chisquare/__main__.py
+++
@@ -1,6 +0,0 @@
-
-from unittest import TextTestRunner
-
-from bi.tests.chisquare import suite
-
-TextTestRunner(verbosity=2).run(suite())
