b7df62bd699807a5c238bbc33367e3c5506e6e38
Srinidhi-SA <Srinidhi.Anantharamu@marlabs.com>
2021-04-08 14:56:54 +0530

    Test push

--- apiCode.sh
+++ apiCode.sh
@@ -5,7 +5,7 @@ cp -r dist/marlabs_bi_jobs-0.0.0-py3.6.egg SPARK_DOCKER/
 ls -l SPARK_DOCKER/
 cd API-CODE
 rm -r mAdvisor-api
-git clone -b master_api_ui --single-branch https://Srinidhi-SA:Sri160293@github.com/Marlabs1/mAdvisor-api
+git clone -b main --single-branch https://Srinidhi-SA:Sri160293@github.com/Srinidhi-SA/temp_api
 cd mAdvisor-api
 rsync -r * ../../SPARK_DOCKER/code/mAdvisor-api/ --exclude API_DOCKER --exclude copyApiFolder.sh --exclude buildspec.yml --exclude hadoop_docker --exclude NGINX_DOCKER --exclude copyHadoopImage.sh --exclude requirements
 cp -r requirements ../../SPARK_DOCKER/requirements/
